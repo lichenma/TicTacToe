936cab0fc4170e4c9d8ee5a2676f76e87f50a65a
Lichen Ma <lichenma123@gmail.com>
2019-03-17 16:01:00 -0400

    update

--- TicTacToe/src/main/webapp/js/GameModule.js
+++ TicTacToe/src/main/webapp/js/GameModule.js
@@ -43,6 +43,105 @@ gameModule.controller('gamesToJoinController', ['$scope', '$http', '$location',
 
         scope.gamesToJoin=[];
 
-        
+        http.get('/game/list').success(function (data) {
+            scope.gamesToJoin=data;
+        }).error(function (data, status, headers, config) {
+            location.path('/player/panel');
+        });
 
+        scope.joinGame=function (id) {
+            var params={"id":id}
+
+            http.post('/game/join', params, {
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8'
+                }
+            }).success(function (data) {
+                location.path('/game/'+data.id);
+            }).error(function (data, status, headers, config) {
+                location.path('/player/panel');
+            });
+        }
+    }]);
+
+gameModule.controller('playerGamesController', ['$scope', '$http', '$location', '$routeParams',
+    function (scope, http, location, routeParams) {
+
+        scope.playerGames= [];
+
+        http.get('/game/player/list').success(function (data) {
+            scope.playerGames = data;
+        }).error(function (data, status, headers, config) {
+            location.path('/player/panel');
+        });
+
+        scope.loadGame = function (id) {
+            console.log(id);
+            location.path('/game/'+id);
+        }
+    }]);
+
+gameModule.controller('gameController', ['$rootScope', '$routeParams', '$scope', '$http',
+    function (rootScope, routeParams, scope, http) {
+
+        var gameStatus;
+        getInitialData()
+
+            function getInitialData() {
+                http.get('/game/'+routeParams.id).success(function (data) {
+                    scope.gameProperties= data;
+                    gameStatus= scope.gameProperties.gameStatus;
+                    getMoveHistory();
+                }).error(function (data, status, headers, config) {
+                    scope.errorMessage="Failed to load game properties";
+                });
+            }
+
+            function getMoveHistory() {
+                scope.movesInGame= [];
+
+                return http.get('/move/list').success(function (data) {
+                    scope.movesInGame=data;
+                    scope.playerMoves=[];
+
+                    //fill the board with positions from the retrieved moves
+                    angular.forEach(scope.movesInGame, function (move) {
+                        scope.rows[move.boardRow-1][move.boardColumn-1].letter = move.playerPieceCode;
+                    });
+                }).error(function (data, status, headers, config) {
+                    scope.errorMessage= "Failed to load moves in game"
+                });
+            }
+
+            function checkPlayerTurn() {
+                return http.get('/move/turn').success(function (data) {
+                    scope.playerTurn=data;
+                }).error(function (data, status, headers, config) {
+                    scope.errorMessage="Failed to get the player turn"
+                });
+            }
+
+            function getNextMove() {
+                scope.nextMoveData=[]
+
+                // Computer is a second player
+                if(!scope.gameProperties.secondPlayer) {
+                    http.get("/move/autocreate").success(function (data, status, headers, config) {
+                        scope.nextMoveData= data;
+                        getMoveHistory().success(function () {
+                            var gameStatus = scope.movesInGame[scope.movesInGame.length-1].gameStatus;
+                            if (gameStatus!='IN_PROGRESS') {
+                                alert(gameStatus)
+                            }
+                        });
+                    }).error(function (data, status, headers, config) {
+                        scope.errorMessage="Can't send move"
+                    });
+                }
+
+                // Second player is a user
+                else {
+                    
+                }
+            }
     }])
