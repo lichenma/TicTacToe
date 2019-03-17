ed7d6af7cdfd6148654faa15f56aac35f7f69517
Lichen Ma <lichenma123@gmail.com>
2019-03-17 15:03:51 -0400

    update

---
+++ TicTacToe/src/main/webapp/js/GameModule.js
@@ -0,0 +1,48 @@
+var gameModule=angular.module('gameModule',[]);
+
+gameModule.controller('newGameController', ['$rootScope','$scope', '$http', '$location',
+    function (rootScope, scope, http, location) {
+
+        rootScope.gameId = null;
+        scope.newGameData = null;
+
+        scope.newGameOptions = {
+            availablePieces: [
+                {name: 'X'},
+                {name: 'O'}
+            ],
+            selectedPiece: {name: 'O'},
+            availableGameTypes: [
+                {name: 'COMPETITION'},
+                {name: 'COMPUTER'}
+            ],
+            selectedBoardDimension: {name: 'COMPUTER'}
+        };
+
+        scope.createNewGame = function () {
+
+            var data = scope.newGameData;
+            var params = JSON.stringify(data);
+
+            http.post("/game/create", params, {
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8'
+                }
+            }).success(function (data, status, headers, config) {
+                rootScope.gameId = data.id;
+                location.path('/game/' + rootScope.gameId);
+            }).error(function (data, status, headers, config) {
+                location.path('/player/panel');
+            });
+        }
+
+    }]);
+
+gameModule.controller('gamesToJoinController', ['$scope', '$http', '$location',
+    function (scope, http, location) {
+
+        scope.gamesToJoin=[];
+
+        
+
+    }])
