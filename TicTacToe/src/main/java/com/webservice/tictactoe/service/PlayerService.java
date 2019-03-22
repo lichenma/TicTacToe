f4a7c97c56ab35d01c0fd5af30a68fe053eef036
Lichen Ma <lichenma123@gmail.com>
2019-03-22 09:55:10 -0400

    update

--- TicTacToe/src/main/java/com/webservice/tictactoe/service/PlayerService.java
+++ TicTacToe/src/main/java/com/webservice/tictactoe/service/PlayerService.java
@@ -1,4 +1,50 @@
 package com.webservice.tictactoe.service;
 
+import com.webservice.tictactoe.DTO.PlayerDTO;
+import com.webservice.tictactoe.domain.Player;
+import com.webservice.tictactoe.repository.PlayerRepository;
+import com.webservice.tictactoe.security.ContextUser;
+import org.aspectj.apache.bcel.util.Play;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
 public class PlayerService {
+
+    private final PlayerRepository playerRepository;
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Autowired
+    public PlayerService(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    public Player createNewPlayer(PlayerDTO playerDTO) {
+
+        Player newPlayer = new Player();
+        newPlayer.setUserName(playerDTO.getUserName());
+        newPlayer.setPassword(passwordEncoder.encode(playerDTO.getPassword()));
+        newPlayer.setEmail(playerDTO.getEmail());
+        playerRepository.save(newPlayer);
+        return newPlayer;
+    }
+
+    public Player getLoggedUser() {
+
+        ContextUser principal = (ContextUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return playerRepository.findOneByUserName(principal.getPlayer().getUserName());
+    }
+
+    public List<Player> listPlayers() {
+        List<Player> players = (List<Player>) playerRepository.findAll();
+        return players;
+    }
+
 }
