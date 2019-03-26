7d3c09ad187cee79c8d7bd4675acd37eb6988953
Lichen Ma <lichenma123@gmail.com>
2019-03-26 14:27:07 -0400

    working version

--- TicTacToe/src/main/webapp/js/application.js
+++ TicTacToe/src/main/webapp/js/application.js
@@ -4,19 +4,19 @@ ticTacToe.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.
         when('/about', {
             templateUrl: 'templates/about.html'
-        }),
+        }).
         when('/player/create', {
-            templateUrl: 'templates/game-board.html',
-            controller: 'gameController'
-        }),
+            templateUrl: 'templates/create.html',
+            controller: 'PlayerCreateController'
+        }).
         when('/game/:id', {
             templateUrl: 'templates/game-board.html',
-            controller: 'newGameController'
-        }),
+            controller: 'gameController'
+        }).
         when('/player/panel', {
             templateUrl: 'templates/player-panel.html',
             controller: 'newGameController'
-        }),
+        }).
         otherwise({
             redirectTo: '/player/panel'
         });
